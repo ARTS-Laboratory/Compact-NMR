a5cca63f70c9a3ad4895788f46b07ababb5dceea
jsmartin10 <jsmartin@email.sc.edu>
2022-04-06 14:55:05 -0400

    General updates
    PCBs & Matlab processing edits

--- System_development/V_2/Data Processing/T2_Curve_Analysis_V6.m
+++ System_development/V_2/Data Processing/T2_Curve_Analysis_V6.m
@@ -1,10 +1,10 @@
 %% Parameters & Curve Extraction
 clc
 clear all
-file1 = '2021-11-12 12-27 Oscilloscope - Waveform Data - 50-50 Hep-Tol (32 Scans - 5s)';
-file2 = '2022-2-16 17-12 Oscilloscope - Waveform Data - Fire Ash Water (A131) (32 Scans - 5s)';
-file3 = '2022-2-16 15-44 Oscilloscope - Waveform Data - Distilled Water (32 Scans - 5s)';
-file4 = '2021-12-09 16-52 Oscilloscope - Waveform Data - Creek Water (June 29) (32 Scans - 5s)';
+file1 = '2022-3-23 17-42 Oscilloscope - Waveform Data - B-AS2-9 (16 Scans - 5s)';
+file2 = '2022-4-01 16-03 Oscilloscope - Waveform Data - B-AS2-9 - MNPs Removed (16 Scans - 5s)';
+file3 = '2022-4-01 16-03 Oscilloscope - Waveform Data - B-AS2-9 - MNPs Removed (16 Scans - 5s)';
+file4 = '2022-4-01 16-03 Oscilloscope - Waveform Data - B-AS2-9 - MNPs Removed (16 Scans - 5s)';
 
 f1name = file1(48:size(file1,2)-16);
 f2name = file2(48:size(file2,2)-16);
@@ -23,10 +23,10 @@ Z1 = readmatrix(file1);
 Z2 = readmatrix(file2);
 Z3 = readmatrix(file3);
 Z4 = readmatrix(file4);
-X1 = Z1(250:1560000,2);
-X2 = Z2(250:1560000,2);
-X3 = Z3(250:1560000,2);
-X4 = Z4(250:1560000,2);
+X1 = Z1(180:1560000,2);
+X2 = Z2(180:1560000,2);
+X3 = Z3(180:1560000,2);
+X4 = Z4(180:1560000,2);
 traw = (0:d/(size(X1,1)):d)';
 traw = traw(1:size(X1,1),1);
 Y1 = zeros(numEchos,1);
@@ -35,48 +35,36 @@ Y3 = zeros(numEchos,1);
 Y4 = zeros(numEchos,1);
 
 % Grabs the peak voltage of every echo %
+k = 1;
 for c = 1:numEchos
-    i = 160;    %Window size
-    j = 379;    %Step size
-    w = 365;    %Correction parameter
-    q = 330;    %Correction parameter
-    k = c*j;    %Step
-    for u=0:10
-        if c > q + w*u
-            k = k-150;
-        end
-        if c > 2885 && c < 2910
-            k = k+10;
-        end
-        if c > 3250 && c < 3280
-            k = k+10;
-        end
-        if c > 3615 && c < 3645
-            k = k+10;
-        end
-    end
-    max1 = X1(k,:);
-    max2 = X2(k,:);
-    max3 = X3(k,:);
-    max4 = X4(k,:);
-    for g = k:(k+i)
-        if X1(g+1,:) > max1
-           max1 = X1(g+1,:);
-        end
-        if X2(g+1,:) > max2
-           max2 = X2(g+1,:);
-        end
-        if X3(g+1,:) > max3
-           max3 = X3(g+1,:);
-        end
-        if X4(g+1,:) > max4
-           max4 = X4(g+1,:);
+    for n = k:k+100       
+        if X1(n,:) > 1.17   % looks for the begining of each RF pulse
+            max1 = X1(n+75,:);
+            max2 = X2(n+75,:);
+            max3 = X3(n+75,:);
+            max4 = X4(n+75,:);
+            for g = (n+75):(n+275) % Scans window after each RF pulse for the echo peak
+                if X1(g+1,:) > max1
+                    max1 = X1(g+1,:);                 
+                end
+                if X2(g+1,:) > max2
+                    max2 = X2(g+1,:);
+                end
+                if X3(g+1,:) > max3
+                    max3 = X3(g+1,:);
+                end
+                if X4(g+1,:) > max4
+                    max4 = X4(g+1,:);
+                end             
+            end          
+            Y1(c,:) = max1;
+            Y2(c,:) = max2;
+            Y3(c,:) = max3;
+            Y4(c,:) = max4;
+            k = n + 300;
+            break
         end
     end
-    Y1(c,:) = max1;
-    Y2(c,:) = max2;
-    Y3(c,:) = max3;
-    Y4(c,:) = max4;
 end
 
 Yraw = zeros(numEchos,4);
@@ -92,12 +80,13 @@ Yd = Y4;
 
 %Filters out noisy components
 for i=1:2
-    Ya(1:numEchos,1) = movmean(Ya(1:numEchos,1),2);
-    Yb(1:numEchos,1) = movmean(Yb(1:numEchos,1),2);
-    Yc(1:numEchos,1) = movmean(Yc(1:numEchos,1),2);
-    Yd(1:numEchos,1) = movmean(Yd(1:numEchos,1),2);
+    Ya(1:numEchos,1) = movmean(Ya(1:numEchos,1),5);
+    Yb(1:numEchos,1) = movmean(Yb(1:numEchos,1),5);
+    Yc(1:numEchos,1) = movmean(Yc(1:numEchos,1),5);
+    Yd(1:numEchos,1) = movmean(Yd(1:numEchos,1),5);
 end
 
+% Gets rid of erroneous data points at the beginning
 Yarr = zeros(numEchos-19,5);
 Yarr(:,1) = t(20:numEchos,1);
 Yarr(:,2) = Ya(20:numEchos,1);
@@ -105,12 +94,18 @@ Yarr(:,3) = Yb(20:numEchos,1);
 Yarr(:,4) = Yc(20:numEchos,1);
 Yarr(:,5) = Yd(20:numEchos,1);
 
+for q=2:5
+    Yarr(:,q) = Yarr(:,q)-min(Yarr(:,q));
+    %Ynorm(:,q) = Ynorm(:,q)-min(Ynorm(:,q));
+end
+
+% Normalizes the decay curves to start at 1
 Ynorm = zeros(numEchos-19,5);
-Ynorm(:,1) = t(20:numEchos,1);
-Ynorm(:,2) = Ya(20:numEchos,1)/max(Ya(20:numEchos,1));
-Ynorm(:,3) = Yb(20:numEchos,1)/max(Yb(20:numEchos,1));
-Ynorm(:,4) = Yc(20:numEchos,1)/max(Yc(20:numEchos,1));
-Ynorm(:,5) = Yd(20:numEchos,1)/max(Yd(20:numEchos,1));
+Ynorm(:,1) = Yarr(:,1);
+
+for j = 2:5
+    Ynorm(:,j) = Yarr(:,j)/max(Yarr(:,j));
+end
 
 %Interpolates data to be 10 times original length/size
 %{
@@ -122,35 +117,38 @@ YIarr(1:size*10,4) = interp1(t,Yarr(:,4),tq,'spline');
 %}
 
 %Fits data to exponential curve
-f1 = fit(t,Ya,'exp2');
-f2 = fit(t,Yb,'exp2');
-f3 = fit(t,Yc,'exp2');
-f4 = fit(t,Yd,'exp2');
+%{
+f1 = fit(t,Ya,'exp1');
+f2 = fit(t,Yb,'exp1');
+f3 = fit(t,Yc,'exp1');
+f4 = fit(t,Yd,'exp1');
 
 %Extracts coefficients from curve fitting
-f1coeff = coeffvalues(f1); f1sd = confint(f1);
-f2coeff = coeffvalues(f2); f2sd = confint(f2); 
-f3coeff = coeffvalues(f3); f3sd = confint(f3);
-f4coeff = coeffvalues(f4); f4sd = confint(f4);
+f1coeff = coeffvalues(f1);
+f1sd = confint(f1);
+f2coeff = coeffvalues(f2); 
+f2sd = confint(f2); 
+f3coeff = coeffvalues(f3); 
+f3sd = confint(f3);
+f4coeff = coeffvalues(f4);
+f4sd = confint(f4);
+
+t21 = 1/f1coeff(1,2);
+t22 = 1/f2coeff(1,2);
+t23 = 1/f3coeff(1,2);
+t24 = 1/f4coeff(1,2);
+%}
 
 %Builds T2 distributions from curve fitting parameters
+%{
 t2 = (0:0.0001:1.5)';
 d1 = normpdf(t2,-1/f1coeff(1,2),-1/f1coeff(1,2)+1/f1sd(1,2)) + normpdf(t2,-1/f1coeff(1,4),-1/f1coeff(1,4)+1/f1sd(1,4));
 d2 = normpdf(t2,-1/f2coeff(1,2),-1/f2coeff(1,2)+1/f2sd(1,2)) + normpdf(t2,-1/f2coeff(1,4),-1/f2coeff(1,4)+1/f2sd(1,4));
 d3 = normpdf(t2,-1/f3coeff(1,2),-1/f3coeff(1,2)+1/f3sd(1,2)) + normpdf(t2,-1/f3coeff(1,4),-1/f3coeff(1,4)+1/f3sd(1,4));
 d4 = normpdf(t2,-1/f4coeff(1,2),-1/f4coeff(1,2)+1/f4sd(1,2)) + normpdf(t2,-1/f4coeff(1,4),-1/f4coeff(1,4)+1/f4sd(1,4));
+%}
 
 %% Plots
-
-%Test Functions
-Ys1 = 0.18*exp(-t/1.6)+0.178*exp(-t/1.18)+0.22*exp(-t/1.15)+0.43*exp(-t/0.88);
-Ys2 = 0.4*exp(-t/1.21479)+0.63096*exp(-t/0.85484);
-Ys3 = 0.6*exp(-t/1.12966)+0.43086*exp(-t/0.80034);
-Ys4 = 0.8*exp(-t/1.06966)+0.23083*exp(-t/0.71097);
-Ys5 = 0.9*exp(-t/1.04049)+0.13089*exp(-t/0.62022);
-
-Yarr(2500:2550,4) = Yarr(2450:2500,4);
-Yarr(2135:2165,4) = Yarr(2105:2135,4);
 
 % Integral of Averaged Data %
 %{
@@ -179,12 +177,11 @@ for c = 1:size
 end
 %}
 
-%plot(t,Y1,'b',t,Y2,'k',t,Y3,'r',t,Y4,'m')
-plot(Yarr(:,1),Yarr(:,2),'b',Yarr(:,1),Yarr(:,3),'k',Yarr(:,1),Yarr(:,4),'r',Yarr(:,1),Yarr(:,5),'c')
-%plot(Yarr(:,1),Ynorm(:,2),'b',Yarr(:,1),Ynorm(:,3),'k',Yarr(:,1),Ynorm(:,4),'r',Yarr(:,1),Ynorm(:,5),'c')
-axis([0 5 0 0.7])  
+%plot(Yarr(:,1),Yarr(:,2),'b',Yarr(:,1),Yarr(:,3),'k',Yarr(:,1),Yarr(:,4),'r',Yarr(:,1),Yarr(:,5),'m')
+plot(Yarr(:,1),Ynorm(:,2),'b',Yarr(:,1),Ynorm(:,3),'k')
+axis([0 4 0 1])  
 grid
-legend(f1name,f2name,f3name,f4name)
+legend(f1name,f2name)
 title('T2 Relaxation Curves')
 xlabel('Time (s)')
 ylabel('Voltage (V)')

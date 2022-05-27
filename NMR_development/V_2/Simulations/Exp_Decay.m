ebde6e85a2063f61afc3c9fe16bd2b47c585e270
jsmartin10 <jsmartin@email.sc.edu>
2022-05-27 11:51:05 -0400

    New PCB versions
    - Main board (RF routing) V3
    - Regulator board (24V to 12V, 5V, 1,8V)
    - HP Amplifier board V2

--- NMR_development/V_2/Simulations/Exp_Decay.m
+++ NMR_development/V_2/Simulations/Exp_Decay.m
@@ -2,13 +2,13 @@ clc
 clear all
 
 size = 1750;
-d = 2.2;
+d = 5;
 t = (0:d/(size-10):d)';
 fs = (size-10)/d;
 T21 = 1.1;
-T22 = 0.5;
-T23 = 1;
-mO = 0.5*exp(-t./T21) + 0.5*exp(-t./T22);
+T22 = 0.9;
+T23 = 1.3;
+mO = 0.33*exp(-t./T21) + 0.33*exp(-t./T22) + 0.33*exp(-t./T22);
 
 Y = zeros(size-9,1);
 
@@ -20,7 +20,7 @@ f1 = fit(t,Y,'exp2')
 
 plot(t,Y)
 grid
-axis([0 2.2 0 1])
+axis([0 5 0 1])
 grid 
 title('Synthetic Signal with Noise')
 xlabel('Time (s)')
